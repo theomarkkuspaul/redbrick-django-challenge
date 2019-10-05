ab683cf5120c2671d2514195697319426172b9c7
Theo Paul <theo.markkus.paul@gmail.com>
2019-10-06 08:48:11 +1300

    Admin import / export tool
    Changes:
    * Wire up import_export tool routing and configuration
    * Add upload template for import_export admin
    * include extra Django cruft

--- books/views.py
+++ books/views.py
@@ -1,9 +1,14 @@
+# Django configs
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-
+# models
 from .models import Book
+
+# etc
+from tablib import Dataset
+
 
 # Create your views here.
 
@@ -17,3 +22,17 @@ def index(request):
   }
 
   return HttpResponse(template.render(context, request))
+
+def upload(request):
+  if request.method == 'POST':
+    book_resource = BookResource()
+    dataset = Dataset()
+    csv_file = request.FILES['myfile']
+
+    imported_data = dataset.load(csv_file.read())
+    result = book_resource.import_data(dataset, dry_run=True)  # Test the data import
+
+    if not result.has_errors():
+      book_resource.import_data(dataset, dry_run=False)  # Actually import now
+
+  return render(request, 'books/upload.html')
